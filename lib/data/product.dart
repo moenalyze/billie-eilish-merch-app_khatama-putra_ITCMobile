075e2b1e5bbfd098dbed34a7be0a9d781ef13f5c
Khatama <152944169+moenalyze@users.noreply.github.com>
2025-02-05 21:38:01 +0700

    added products

--- lib/data/product.dart
+++ lib/data/product.dart
@@ -30,7 +30,7 @@ class Product {
 final List<Product> products = [
   Product(
     name: 'HIT ME HARD AND SOFT Excl. Vinyl',
-    price: 29.99 * 16000,
+    price: 37.99 * 16000,
     description: 'Made of 100% recycled black vinyl',
     imagePath: 'assets/images/hmhas_vinyl.png',
     quantity: 0,
@@ -38,7 +38,7 @@ final List<Product> products = [
   ),
   Product(
     name: 'Blue & Red Zip Hoodie',
-    price: 99.00 * 16000,
+    price: 125.00 * 16000,
     description: 'Produced using 100% organic cotton',
     imagePath: 'assets/images/hmhas_hoodie.png',
     quantity: 0,
@@ -46,25 +46,49 @@ final List<Product> products = [
   ),
   Product(
     name: 'HIT ME HARD AND SOFT Standard CD',
-    price: 12.99 * 16000,
+    price: 16.99 * 16000,
     description: 'Made of 100% recycled black CD',
     imagePath: 'assets/images/hmhas_cd.png',
     quantity: 0,
     type: 'hmhas'
   ),
   Product(
-    name: 'Hit Me Hard And Soft Blue Blohsh Necklace',
-    price: 60.00 * 16000,
+    name: 'HIT ME HARD AND SOFT Blue Blohsh Necklace',
+    price: 78.00 * 16000,
     description: 'Sterling Silver Rhinestone Blohsh pendant. 16” Chain with 3” extender',
     imagePath: 'assets/images/hmhas_necklace.png',
     quantity: 0,
     type: 'hmhas'
   ),
   Product(
-    name: 'HAPPIER THAN EVER',
-    price: 60.00 * 16000,
-    description: 'Sterling Silver Rhinestone Blohsh pendant. 16” Chain with 3” extender',
-    imagePath: 'assets/images/hmhas_necklace.png',
+    name: 'Blohsh Polo Long Sleeve',
+    price: 106.00 * 16000,
+    description: 'Produced using: 100% recycled cotton',
+    imagePath: 'assets/images/new_polo.png',
+    quantity: 0,
+    type: 'newCollection'
+  ),
+  Product(
+    name: 'L\'amour Caribiner Clip Set',
+    price: 13.00 * 16000,
+    description: 'Produced using: 100% aluminum/metal',
+    imagePath: 'assets/images/new_clip-set.png',
+    quantity: 0,
+    type: 'newCollection'
+  ),
+  Product(
+    name: 'Thermal Be Collage Photo Tee',
+    price: 44.00 * 16000,
+    description: 'Produced using: 100% recycled cotton',
+    imagePath: 'assets/images/new_t-shirt.png',
+    quantity: 0,
+    type: 'newCollection'
+  ),
+  Product(
+    name: 'HIT ME HARD AND SOFT Mesh Shorts',
+    price: 69.00 * 16000,
+    description: 'Produced using: 100% recycled polyester',
+    imagePath: 'assets/images/new_shorts.png',
     quantity: 0,
     type: 'newCollection'
   ),
